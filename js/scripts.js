2f625e2bd9cb8a3c48e333d42cc737c731f23781
Jarrod Green and Vilija Jozaitis <vilija@>
2019-07-01 20:27:01 -0700

    set up Javascript functions to calculate balance after initial deposit and withdrawal

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,47 @@
 function UserDetails(name, initialDeposit) {
   this.name = name
   this.initialDeposit = initialDeposit
-}
+};
+
+UserDetails.prototype.Adjust = function () {
+  return this.currentBalance += (this.newDeposit - this.newWithdrawal);
+};
+
+function resetFields() {
+  $("#new-deposit").val("");
+  $("#new-withdrawal").val("");
+};
 
 var myUserDetails = new UserDetails();
 
 $(document).ready(function() {
     $("form#register").submit(function(event) {
-            debugger
+            // debugger
       event.preventDefault();
       var initialDeposit = $("input#initial-deposit").val();
       var currentBalance = initialDeposit
 
+      $("#currentBalance").text(currentBalance);
+
       console.log(initialDeposit)
     })
 
+      var deposit = parsInt($("#new-deposit").val());
+      var withdrawal = parseInt($("#new-withdrawal").val());
+      deposit = deposit || 0
+      withdrawal = withdrawal || 0
+      userAccount.newDeposit = deposit;
+      userAccount.newWithdrawal = withdrawal;
 
-    function displayCurrentBalance(currentBalance) {
-      var  = $("ul#contacts");
-      var htmlForContactInfo = "";
-      addressBookToDisplay.contacts.forEach(function(contact) {
+      userAccount.Adjust();
 
-      });
-      contactsList.html(htmlForContactInfo);
+      $("#account-balance").text(userAccount.balance);
+
+      resetFields();
+    });
+
+    $("#account-name").text(userAccount.name);
+    $("#account-balance").text(userAccount.balance);
+
   });
 });
